a63e798f55af38db0305334e2ad1454d653799fc
Naruebet3001 <142212146+Naruebet3001@users.noreply.github.com>
2025-09-15 17:30:03 +0700

    Update training_worker.py

--- training_worker.py
+++ training_worker.py
@@ -1,1 +1,18 @@
+import os, subprocess, sys
+
+# เก็บ pid ของตัวเอง
+with open("pid.txt", "w") as f:
+    f.write(str(os.getpid()))
+
+# run yolov8
+cmd = [
+    "yolo", "detect", "train",
+    "data=datasets/data.yaml",
+    "model=yolov8n.pt",
+    "epochs=5", "imgsz=640",
+    "project=runs", "name=train"
+]
+
+subprocess.run(cmd)
+
 
